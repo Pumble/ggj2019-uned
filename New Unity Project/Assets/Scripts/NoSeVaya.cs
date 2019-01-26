f20a75e943b5aec5fd72067183dc3e443a008b8b
Pumble <juanlgu27@gmail.com>
2019-01-26 00:58:04 -0600

    Arte de la isla incluido
    Se agregaron 4 fondos nuevos

---
+++ New Unity Project/Assets/Scripts/NoSeVaya.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://docs.unity3d.com/ScriptReference/Collider2D.html
+// https://docs.unity3d.com/ScriptReference/Component-gameObject.html
+// https://docs.unity3d.com/ScriptReference/GameObject.html
+
+public class NoSeVaya : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.name == "player")
+        {
+            if (col.gameObject.transform.position.x < -0.1)
+            {
+                // SALIENDOSE POR LA IZQUIERDA
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+            }
+            if (col.gameObject.transform.position.x < 0.1)
+            {
+                // SALIENDOSE POR LA DERECHA
+                transform.Translate(Vector2.left * speed * Time.deltaTime);
+            }
+            if (col.gameObject.transform.position.y > 0.1)
+            {
+                // SALIENDOSE POR ARRIBA
+                transform.Translate(Vector2.down * speed * Time.deltaTime);
+            }
+            if (col.gameObject.transform.position.y > -0.1)
+            {
+                // SALIENDOSE POR ABAJO
+                transform.Translate(Vector2.up * speed * Time.deltaTime);
+            }
+
+            Debug.Log(col.gameObject.transform.position.x);
+            Debug.Log(col.gameObject.transform.position.y);
+
+            //Debug.Log(col.OverlapPoint().ToStiring());
+            //transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
