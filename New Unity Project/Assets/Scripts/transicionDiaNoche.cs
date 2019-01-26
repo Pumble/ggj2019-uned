6bb2026795ed875906fd0869504f64ef8f91129f
Pumble <juanlgu27@gmail.com>
2019-01-26 07:10:57 -0600

    Preparando cambios para la camara
    Preparando cambios para la camara

--- New Unity Project/Assets/Scripts/transicionDiaNoche.cs
+++ New Unity Project/Assets/Scripts/transicionDiaNoche.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 // https://docs.unity3d.com/ScriptReference/MonoBehaviour.InvokeRepeating.html
 // https://www.youtube.com/watch?v=PuXUCX21jJU
@@ -13,20 +14,26 @@ public class transicionDiaNoche : MonoBehaviour
     public Sprite fondo2;
     public Sprite fondo3;
     public Sprite fondo4;
-    private Sprite[] archivosFondo;
     public SpriteRenderer spriteRenderer;
-
     private int archivoFondoIndice = 0;
-
     public float tiempoTransicion = 10f; // 10
+    public float tiempoParaReiniciarCamara = 5f;
+    public Camera m_OrthographicCamera;
 
+    public CinemachineVirtualCamera vcam;
+
+    private LensSettings defaultLens;
+    private LensSettings zoomOutLens;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("cambiarFondo", 1.0f, tiempoTransicion);
         // INICIA CON UN SEGUNDO DESPUES Y SE EJECUTA CADA 30s
-    }
+
+        defaultLens = new LensSettings(19.6f, 2f, 0, 01f, 5000f, 0f, true, );
+        zoomOutLens = new LensSettings(19.6f, 4f, 0, 01f, 5000f, 0f, true, );
+}
 
     // Update is called once per frame
     void Update()
@@ -36,7 +43,6 @@ public class transicionDiaNoche : MonoBehaviour
 
     void cambiarFondo()
     {
-        Debug.Log("Cambiando el fondo"); 
         switch(archivoFondoIndice)
         {
             case 0:
@@ -56,5 +62,15 @@ public class transicionDiaNoche : MonoBehaviour
                 break;
         }
         archivoFondoIndice++;
+        // m_OrthographicCamera.orthographicSize = 4f;
+        vcam.m_Lens = zoomOutLens;
+        //StartCoroutine(reiniciarCamara());
+    }
+    IEnumerator reiniciarCamara() {
+        // AQUI SOLO DEBEMOS VOLVER LA CAMARA A LA ORIGINALIDAD
+        print(Time.time);
+        m_OrthographicCamera.orthographicSize = 2.0f;
+        yield return new WaitForSeconds(tiempoParaReiniciarCamara);
+        print(Time.time);
     }
 }
