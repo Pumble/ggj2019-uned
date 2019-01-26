c580e373185c9dd48d8e91cf732e4d9f15c3b2e4
Joseph Zamora <joseph.zamora64@yahoo.com>
2019-01-26 02:44:07 -0600

    Intentar unir las escenas

---
+++ New Unity Project/Assets/Scripts/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+	[SerializeField]
+	private GameObject lobo;
+
+	[SerializeField]
+	private int cuantosLobos;
+
+	[SerializeField]
+	[Tooltip("Tiempo para que los lobos spawneen")]
+	private float tiempoSpawn;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (cuantosLobos > 0)
+		{
+			StartCoroutine(spawnearLobos());
+			cuantosLobos--;
+		}
+    }
+
+	IEnumerator spawnearLobos()
+	{
+		yield return new WaitForSeconds(tiempoSpawn);
+		Instantiate(lobo);
+	}
+}
